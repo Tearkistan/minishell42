70561452bd69157f050a9d4f24706f4d4880ea7a
Tearkistan <teaguewatson@gmail.com>
2026-01-06 14:22:16 +0100

    base directory strucuture including header and Makefile added, Patrycja will add her libft

---
+++ Makefile
@@ -0,0 +1,43 @@
+NAME=minishell
+
+SRC_DIR=src
+INC_DIR=inc
+OBJ_DIR=obj
+
+LIBFT_DIR=libft
+
+LIBFT=$(LIBFT_DIR)/libft.a
+
+CC=cc
+CFLAGS=-Wall -Wextra -Werror -I$(LIBFT_DIR) -I$(INC_DIR) 
+
+SRC=
+
+SRCS= $(addprefix $(SRC_DIR)/,$(SRC))
+
+OBJS=$(SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
+
+all: $(LIBFT) $(NAME)
+
+$(NAME): $(OBJS)
+	@mkdir -p $(OBJ_DIR)
+	$(CC) $(CFLAGS) $(OBJS) $(LIBFT) -o $(NAME)
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	$(MAKE) -C $(LIBFT_DIR) clean
+	rm -rf $(OBJ_DIR)
+
+fclean: clean
+	$(MAKE) -C $(LIBFT_DIR) fclean
+	rm -rf $(NAME) $(BONUS)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- inc/minishell.h
+++ inc/minishell.h
@@ -1,13 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: twatson <twatson@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/06 13:22:22 by twatson           #+#    #+#             */
-/*   Updated: 2026/01/06 13:22:24 by twatson          ###   ########.fr       */
+/*   Created: 2026/01/06 14:16:20 by twatson           #+#    #+#             */
+/*   Updated: 2026/01/06 14:20:12 by twatson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# include "libft.h"
+
+/* PARSING */
+
+/* EXECUTION */
+
+#endif
