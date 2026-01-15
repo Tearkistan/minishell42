66bae7e2c68b247943468f7e77060ef1a9357b0f
Tearkistan <teaguewatson@gmail.com>
2026-01-15 15:56:54 +0100

    heredoc vs normal mode skeleton added

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: twatson <twatson@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:19:50 by twatson           #+#    #+#             */
-/*   Updated: 2026/01/14 15:03:24 by twatson          ###   ########.fr       */
+/*   Updated: 2026/01/15 15:56:23 by twatson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,21 @@ static int	init_redirects(t_redirects *redir, t_shell *shell, t_pipex *pipex)
 			init_heredoc_mode(pipex, redir);
 		else
 			init_norm_mode(pipex, redir);
-			
-
-
-
+		curr = curr->next;
+	}
+	return (0);
 }
 
 int execute_line(t_pipeline *pipeline, t_shell *shell)
 {
-	t_pipex	pipex;
-
-	while (pipeline->cmd)
+	t_pipex		pipex;
+	t_pipeline	*curr;
+	
+	curr = pipeline
+	while (curr)
 	{
-		init_redirects(pipeline->cmd->redirects, shell, &pipex);
+		init_redirects(curr->cmd->redirects, shell, &pipex);
+		curr = curr->next;
+	}
 	return (0);
 }
